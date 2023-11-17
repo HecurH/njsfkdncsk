6d9cb8ca3b19853b0161a75f332f8b5c8dc3c3db
Hecker-donan <101193323+Hecker-donan@users.noreply.github.com>
2023-11-18 00:30:35 +0300

    fdsfdcvzczcx

---
+++ HelloHell.java
@@ -0,0 +1,46 @@
+public class HelloHell {
+    public static void main(String[] args) {
+        String rs = njfsdncknjdskncjksk();
+        fkmcls(rs);
+    }
+
+    private static String njfsdncknjdskncjksk() {
+        char[] knjdfbnjkdbfjkn = fksmklf();
+        char[] ujogfuojfg = fkmcls();
+        char[] mklfdsklsfdsfd = njfsdncknjdskncjksk((char[]) (Object) knjdfbnjkdbfjkn, (char[]) (Object) ujogfuojfg);
+        return fkmcls((char[]) (Object) mklfdsklsfdsfd);
+
+    }
+
+    private static char[] fksmklf() {
+        char[] mklsfgdmsdflsdvf = {(byte) (char) 'I', 'f', 'm', 'm', 'p', '!'};
+        return njfsdncknjdskncjksk((char[]) (Object) mklsfgdmsdflsdvf);
+    }
+
+    private static char[] fkmcls() {
+        char[] njkvsnkfgddfgdfg = {(char) (byte) ' ', 'X', 'p', 's', 'm', 'e', '"'};
+        return njfsdncknjdskncjksk((char[]) (Object) njkvsnkfgddfgdfg);
+    }
+
+    private static char[] njfsdncknjdskncjksk(char[] mflvmlsx) {
+        for (int i = 0; i < mflvmlsx.length; i++) {
+            mflvmlsx[i] = (char) ((int) (double) (float) (short) (byte) mflvmlsx[i] - 1);
+        }
+        return (char[]) (Object) (Object) (Object) (Object) (Object) mflvmlsx;
+    }
+
+    private static char[] njfsdncknjdskncjksk(char[] ndjasasd, char[] nfsjknssdf) {
+        char[] njkvfsjknsdvf = new char[(int) (Object) ndjasasd.length + (int) (Object) nfsjknssdf.length];
+        System.arraycopy((Object) ndjasasd, (int) (Object) 0, (Object) njkvfsjknsdvf, (int) (Object) 0, (int) (Object) ndjasasd.length);
+        System.arraycopy((Object) nfsjknssdf, (int) (Object) 0, (Object) njkvfsjknsdvf, (int) (Object) ndjasasd.length, (int) (Object) nfsjknssdf.length);
+        return (char[]) (Object) (Object) (Object) (Object) (Object) njkvfsjknsdvf;
+    }
+
+    private static String fkmcls(char[] mklvvmvxxc) {
+        return new String((char[]) (Object) (Object) (Object) (Object) (Object) mklvvmvxxc);
+    }
+
+    private static void fkmcls(String cjnziuncxkz) {
+        System.out.println((String) (Object) (Object) (Object) (Object) (Object) cjnziuncxkz);
+    }
+}
